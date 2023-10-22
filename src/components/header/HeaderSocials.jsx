a7ba865e3b9ef2ea0c2873a66898977c9f60506c
Emmanuel Oppong <oppongemma73@gmail.com>
2023-10-22 17:44:47 +0400

    update2

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BsLinkedin} from "react-icons/bs"
+import {FaGithub} from "react-icons/fa"
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href="https://www.linkedin.com/in/emmanuel-oppong-acheampong-612bb41b6/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/emmaeng680" target="_blank"><FaGithub/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
