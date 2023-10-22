84b83dcf30e84eba6dcffc3b8b43bd4296a4adba
Emmanuel Oppong <oppongemma73@gmail.com>
2023-10-23 00:58:16 +0400

    update8

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-      <div className="container header__container">
+      <div className="container header__container" id ='home'>
         <h5>Hello I'm</h5>
         <h1>Emmanuel Oppong</h1>
         <h5 className="text-light">Frontend Developer</h5>
